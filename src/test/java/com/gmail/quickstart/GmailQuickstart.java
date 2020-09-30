879ec4ca02105cd450716e8f40a82fce79668187
kieran <kieransnipp@gmail.com>
2020-09-30 15:57:06 +0100

    gmail first part done

--- src/test/java/com/gmail/quickstart/GmailQuickstart.java
+++ src/test/java/com/gmail/quickstart/GmailQuickstart.java
@@ -15,35 +15,40 @@ import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.Label;
 import com.google.api.services.gmail.model.ListLabelsResponse;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
 
+import io.restassured.path.json.JsonPath;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class GmailQuickstart {
 	private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
 	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static final String USER_ID = "me";
 
 	/**
 	 * Global instance of the scopes required by this quickstart. If modifying these
 	 * scopes, delete your previously saved tokens/ folder.
 	 */
-	private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_LABELS);
+	// private static final List<String> SCOPES =
+	// Collections.singletonList(GmailScopes.GMAIL_LABELS);
+	private static final List<String> SCOPES = Arrays.asList(GmailScopes.MAIL_GOOGLE_COM);
 
 	// Path to credentials file
 	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 	InputStream in = Gmail.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-	
-//	private static final String CREDENTIALS_FILE_PATH = 
-//			System.getProperty("user.dir") + 
-//			File.separator + "src"
-//			+ File.separator + "main" + 
-//			File.separator + "resources" + 
-//			File.separator + "credentials" + 
-//			File.separator+ "credentials.json";
 
 	static final String TOKENS_DIRECTORY_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator
 			+ "main" + File.separator + "resources" + File.separator + "credentials";
@@ -76,23 +81,150 @@ public class GmailQuickstart {
 		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 	}
 
-	public static void main(String... args) throws Exception {
+	public static Gmail getService() throws Exception {
 		// Build a new authorized API client service.
 		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 		Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
 				.setApplicationName(APPLICATION_NAME).build();
+		return service;
+	}
 
-		// Print the labels in the user's account.
-		String user = "me";
-		ListLabelsResponse listResponse = service.users().labels().list(user).execute();
-		List<Label> labels = listResponse.getLabels();
-		if (labels.isEmpty()) {
-			System.out.println("No labels found."); 
-		} else {
-			System.out.println("Labels:");
-			for (Label label : labels) {
-				System.out.printf("- %s\n", label.getName());
+	public static List<Message> listMessagesMatchingQuery(Gmail service, String userId, String query)
+			throws IOException {
+		ListMessagesResponse response = service.users().messages().list(userId).setQ(query).execute();
+		List<Message> messages = new ArrayList<Message>();
+		while (response.getMessages() != null) {
+			messages.addAll(response.getMessages());
+			if (response.getNextPageToken() != null) {
+				String pageToken = response.getNextPageToken();
+				response = service.users().messages().list(userId).setQ(query).setPageToken(pageToken).execute();
+			} else {
+				break;
 			}
 		}
+		return messages;
+	}
+
+	public static Message getMessage(Gmail service, String userId, List<Message> messages, int index)
+			throws IOException {
+		Message message = service.users().messages().get(userId, messages.get(index).getId()).execute();
+		return message;
+	}
+
+	public static HashMap<String, String> getGmailData(String query) {
+		try {
+			Gmail service = getService();
+			List<Message> messages = listMessagesMatchingQuery(service, USER_ID, query);
+			Message message = getMessage(service, USER_ID, messages, 0);
+			JsonPath jp = new JsonPath(message.toString());
+			String subject = jp.getString("payload.headers.find { it.name == 'Subject' }.value");
+			String body = new String(Base64.getDecoder().decode(jp.getString("payload.parts[0].body.data")));
+			String link = null;
+			String arr[] = body.split("\n");
+			for (String s : arr) {
+				s = s.trim();
+				if (s.startsWith("http") || s.startsWith("https")) {
+					link = s.trim();
+				}
+			}
+			HashMap<String, String> hm = new HashMap<String, String>();
+			hm.put("subject", subject);
+			hm.put("body", body);
+			hm.put("link", link);
+			return hm;
+
+		} catch (Exception e) {
+			System.out.println("email not found....");
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static int getTotalCountOfMails() {
+		int size;
+		try {
+			final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+					.setApplicationName(APPLICATION_NAME).build();
+			List<com.google.api.services.gmail.model.Thread> threads = service.users().threads().list("me").execute()
+					.getThreads();
+			size = threads.size();
+		} catch (Exception e) {
+			System.out.println("Exception log " + e);
+			size = -1;
+		}
+		return size;
+	}
+
+	public static boolean isMailExist(String messageTitle) {
+		try {
+			final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+					.setApplicationName(APPLICATION_NAME).build();
+			ListMessagesResponse response = service.users().messages().list("me").setQ("subject:" + messageTitle)
+					.execute();
+			List<Message> messages = getMessages(response);
+			return messages.size() != 0;
+		} catch (Exception e) {
+			System.out.println("Exception log" + e);
+			return false;
+		}
+	}
+
+	private static List<Message> getMessages(ListMessagesResponse response) {
+		List<Message> messages = new ArrayList<Message>();
+		try {
+			final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+					.setApplicationName(APPLICATION_NAME).build();
+			while (response.getMessages() != null) {
+				messages.addAll(response.getMessages());
+				if (response.getNextPageToken() != null) {
+					String pageToken = response.getNextPageToken();
+					response = service.users().messages().list(USER_ID).setPageToken(pageToken).execute();
+				} else {
+					break;
+				}
+			}
+			return messages;
+		} catch (Exception e) {
+			System.out.println("Exception log " + e);
+			return messages;
+		}
+	}
+//	    	public static void main(String... args) throws Exception {
+//			// Build a new authorized API client service.
+//			final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+//			Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+//					.setApplicationName(APPLICATION_NAME).build();
+//	
+//			// Print the labels in the user's account.
+//			String user = "me";
+//			ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+//			List<Label> labels = listResponse.getLabels();
+//			if (labels.isEmpty()) {
+//				System.out.println("No labels found."); 
+//			} else {
+//				System.out.println("Labels:");
+//				for (Label label : labels) {
+//					System.out.printf("- %s\n", label.getName());
+//				}
+//			}
+//		}
+
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
+		HashMap<String, String> hm = getGmailData("subject:Streamxxx");
+		System.out.println("Subject =" + hm.get("subject"));
+		System.out.println("=================");
+		System.out.println("Body =" + hm.get("body"));
+		System.out.println("=================");
+		System.out.println("Link =" + hm.get("link"));
+
+		System.out.println("=================");
+		System.out.println("Total count of emails is :" + getTotalCountOfMails());
+
+		System.out.println("=================");
+		boolean exist = isMailExist("new link");
+		System.out.println("title exist or not: " + exist);
+
 	}
 }
